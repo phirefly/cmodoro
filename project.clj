e53518d25d1e2a0d36fb8c395f6130a9a6209fd1
Raphy Villas <raphael.villas@gmail.com>
2014-12-12 14:13:36 -0500

    Run it: java -jar target/cmodoro-0.1.0-SNAPSHOT-standalone.jar doh

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject cmodoro "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "My first command line app"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main cmodoro.core
+  )
--- src/cmodoro/core.clj
+++ src/cmodoro/core.clj
@@ -1,6 +1,9 @@
-(ns cmodoro.core)
+(ns cmodoro.core
+  (:require [clojure.java.io :as io])
+  (:gen-class :main true))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  (defn -main
+    "The app's main function"
+    [& args]
+    (println "*** Your args: ", args)
+  )
