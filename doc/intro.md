724da318d76a5e2ade9a01a220f1feb50731599f
Raphy Villas <raphael.villas@gmail.com>
2014-12-12 12:47:39 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cmodoro
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cmodoro/core_test.clj
@@ -0,0 +1,7 @@
+(ns cmodoro.core-test
+  (:require [clojure.test :refer :all]
+            [cmodoro.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
